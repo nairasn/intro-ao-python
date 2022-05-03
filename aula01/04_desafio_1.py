6e0de48b55c41b81db429de44c04c97db36eabe3
Naira Nascimento <87916977+nairasn@users.noreply.github.com>
2022-05-03 03:33:48 -0300

    Update 04_desafio_1.py

--- aula01/04_desafio_1.py
+++ aula01/04_desafio_1.py
@@ -4,8 +4,4 @@ idade = input('Qual é sua idade?\n')
 cidade = input('Onde você mora?\n')
 
 # 2. Imprima no console uma única mensagem desejando boas vindas ao usuário.
-<<<<<<< HEAD
 print("Seja muito bem vinda: " + nome + ", que tem " + idade + " anos, e mora em " + cidade + "!!")
-=======
-print("Seja muito bem vinda: " + nome + ", que tem " + idade + " anos, e mora em " + cidade + "!!")
->>>>>>> 3f87d1397b5cb85cfe6469988c2b4145ba6b9901
