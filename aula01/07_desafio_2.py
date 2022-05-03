48843e11d8adc6f58047134dd9b306d05561bb55
nairasn <nascimentosnaira@gmail.com>
2022-05-03 16:24:38 -0300

    nova maneira do desafio aula 01 o 2

--- aula01/07_desafio_2.py
+++ aula01/07_desafio_2.py
@@ -1,4 +1,7 @@
 # 1. Pergunte ao usuário em qual cidade e o estado em que ele(a) mora
+from telnetlib import NAOHTD
+
+
 nome = input('Qual seu nome? \n')
 cidade = input('Em que cidade você mora?\n')
 estado = input('Em que estado você mora?\n')
@@ -12,3 +15,6 @@ print(frase)
 #    Utilize uma forma de formatação de string diferente da que foi usada no item 2.
 funcao_format = ' {} mora no estado de/da/do {} '.format(nome, estado.upper())
 print(funcao_format)
+
+#interpolacao_de_variaveis = f'A {nome} mora no Estado da/do/de {estado.upper()}.'
+#print(interpolacao_de_variaveis)
