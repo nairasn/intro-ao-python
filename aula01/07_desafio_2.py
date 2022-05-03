ec3f4d837a620928a0378d9a93e1f11826a9fbe7
nairasn <nascimentosnaira@gmail.com>
2022-05-03 03:26:01 -0300

    aula 01 desafio 2

--- aula01/07_desafio_2.py
+++ aula01/07_desafio_2.py
@@ -1,20 +1,14 @@
 # 1. Pergunte ao usuário em qual cidade e o estado em que ele(a) mora
+nome = input('Qual seu nome? \n')
 cidade = input('Em que cidade você mora?\n')
 estado = input('Em que estado você mora?\n')
 # 2. Imprima uma mensagem diga a cidade em que o usuário mora. 
 #    O nome da cidade deve estar todo em letras maiúsculas.
-frase = 'Usuária mora em ' + cidade
-print(frase.upper())
-
-concatenacao = 'A usuária' + nome_doguinha + ' tem ' + str(idade_doguinha) + ' anos de idade'
-print(concatenacao)
+frase = nome + ' mora em ' + cidade.upper() + '.'
+print(frase)
 
 # 3. Imprima uma mensagem diga o estado em que o usuário mora. 
 #    O nome do estado deve estar todo em letras maiúsculas.
 #    Utilize uma forma de formatação de string diferente da que foi usada no item 2.
-
-#APAGAR ISSO!
-
-#DAR GIT STAGE ARQUIVO ESQUPECIDO 
-# GIT COMMIT
-# GIT PUSH
+funcao_format = ' {} mora no estado de/da/do {} '.format(nome, estado.upper())
+print(funcao_format)
