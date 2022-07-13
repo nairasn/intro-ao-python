259832da3521b622562d5667af9dfb55a5cef67b
Naira Nascimento <87916977+nairasn@users.noreply.github.com>
2022-07-13 00:15:42 -0300

    Update 03_desafio_5.py

--- aula03/03_desafio_5.py
+++ aula03/03_desafio_5.py
@@ -1,4 +1,12 @@
 # Crie uma lista com 60 números começando com o valor 1 e indo até o número 60 (ou seja, o número 60 deve estar na lista).
-
+lista = list(range(1, 61))
+print(lista)
 
 # Imprima todos os itens da sua lista de índice par. Imprima o índice e o item.
+print(' Índice   *  Item ')
+print(' ------   |  -----')
+
+for i, item in enumerate(lista):
+    if item % 2 == 0:
+        print(f' Indice  {i}| Item {item}')
+print()
